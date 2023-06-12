f87670719a9a2d1b6a7478f8bb2ab2d6f041783e
hungnq2023 <129919799+hungnq2023@users.noreply.github.com>
2023-06-12 23:21:25 +0700

    Ngày 12/06
    Mảng

--- leftmenu.js
+++ leftmenu.js
@@ -1,4 +1,33 @@
 
-function test_btn_click(){
+function test_btn_click() {
     console.log("Test thử");
 }
+
+function hlook() {
+    var value = document.getElementById("fname").value;
+    var nhap = value;
+    if (nhap === "Hung") {
+        alert("Đúng");
+    }
+    else {
+        alert("Sai");
+    }
+}
+var cars = ["BWM", "HONDA", "KIA", "FORD", "MITSU", "MITSU XPANDER"];
+function checkCar(car) {
+    return car > document.getElementById("fname").value; // trả về giá trị car từ textbox nhập
+}
+
+function lstsearch() {
+    var nhap = document.getElementById("fname").value;
+    var nhapcar = nhap;
+    if (nhapcar === cars.filter(checkCar)) {
+        alert("Đúng");
+    }
+    else {
+        alert("sai");
+    }
+}
+
+
+
