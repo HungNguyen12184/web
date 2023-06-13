b2b46c7e133377fce83e3fb0b2f70889b0803fb1
hungnq2023 <129919799+hungnq2023@users.noreply.github.com>
2023-06-13 22:33:41 +0700

    13/06
    Dùng mảng để tìm kiếm

--- leftmenu.js
+++ leftmenu.js
@@ -15,17 +15,48 @@ function hlook() {
 }
 var cars = ["BWM", "HONDA", "KIA", "FORD", "MITSU", "MITSU XPANDER"];
 
-function lstsearch() {
+function lstfilter() {
     var nhap = document.getElementById("fname").value;
     var nhapcar = nhap;
     var listcar = cars.filter(function checkCar(car, index, array) { return car === nhapcar; });
     if (listcar.length > 0) {
-        document.getElementById("fname").innerHTML = listcar;
+        alert("Xe có trong bãi");
     }
     else {
-        alert("sai");
+        alert("Xe bán hết");
     }
 }
 
 
+function lsteach() {
+    var nhap = document.getElementById("fname").value;
+    var nhapcar = nhap;
+    var listcar1 = cars.forEach(function myCar(car, index, array) {
+        return car === nhapcar;
+    });
+    if (listcar1.length > 0) {
+        console.log("Xe có trong hãng", nhapcar);
+    }
+    else {
+        console.log("xe không có trong hãng", nhapcar);
+    }
 
+}
+
+
+
+// function lstsearch() {
+//     var nhap = document.getElementById("fname").value;
+//     var nhapcar = nhap;
+//     var listcar = [];
+//     for (var i = 0; i < cars.length; i++) {
+//         listcar += cars[i];
+//         if (listcar === nhapcar) {
+//             console.log("XE có trong bãi");
+//         }
+//         else {
+//             console.log("XE bán rồi");
+//         }
+//     }
+
+// }
