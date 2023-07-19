3b8eb590e6b4229051d0de999100f6f699d955c1
HungNguyen12184 <135783013+HungNguyen12184@users.noreply.github.com>
2023-07-19 21:49:18 +0700

    19/07
    list month year

--- leftmenu.js
+++ leftmenu.js
@@ -157,107 +157,175 @@ function searchLabel() {
     });
 }
 /*------------------LPR------------------------*/
-function hideModalWhenFullHeightVisible() {
-    var fullHeightElement = document.querySelector('.full-height');
-    var modalElement = document.getElementById('modal-root');
+// function hideModalWhenFullHeightVisible() {
+//     var fullHeightElement = document.querySelector('.full-height');
+//     var modalElement = document.getElementById('modal-root');
 
-    if (fullHeightElement && modalElement) {
-        var fullHeightVisible = window.getComputedStyle(fullHeightElement).display !== 'none';
-        var modalVisible = window.getComputedStyle(modalElement).display !== 'none';
+//     if (fullHeightElement && modalElement) {
+//         var fullHeightVisible = window.getComputedStyle(fullHeightElement).display !== 'none';
+//         var modalVisible = window.getComputedStyle(modalElement).display !== 'none';
 
-        if (fullHeightVisible && modalVisible) {
-            modalElement.style.display = 'none';
-        }
-    }
-}
+//         if (fullHeightVisible && modalVisible) {
+//             modalElement.style.display = 'none';
+//         }
+//     }
+// }
 
-window.onload = hideModalWhenFullHeightVisible;
+// window.onload = hideModalWhenFullHeightVisible;
 
 
 
-// var dtpControl = document.querySelector("dtp-control-container");
-// var mInput = document.querySelector("m-input-moment");  
-// var modalElement = document.getElementById('modal-root');  
- 
-// dtpControl = addEventListener("click", function()
-//   {
-//     modalElement.style.display = "block";
-//   }
-// )
 
 
 
 let currentMonth = new Date().getMonth();
 let currentYear = new Date().getFullYear();
 
-  function displayInfo() {
+function displayInfo() {
     var monthCL = document.querySelector(".month");
     var yearCL = document.querySelector(".year");
     var currentMonthName = new Date(currentYear, currentMonth).toLocaleString(
-      "vi-VN",
-      { month: "long" }
+        "vi-VN",
+        { month: "long" }
     ); /* chuyển đổi 1 dối tượng date thành 1 chuoi bieu dien ngôn ngữ theo cấu hình vùng*/
     monthCL.innerText = currentMonthName;
-  yearCL.innerText = currentYear;
-  renderDate();
+    yearCL.innerText = currentYear;
+    renderDate();
+
+    monthCL.addEventListener("click", function () {
+        showMonthTable();
+    });
+
+    yearCL.addEventListener("click", function () {
+        showYearTable();
+    });
+}
+
+
+function showMonthTable() {
+
+    var monthTable = document.createElement("div");
+    monthTable.className = "m-calendar";
+    monthTable.innerHTML = "<table><tbody></tbody></table>";
+
+    var tableBody = monthTable.querySelector("tbody");
+    var months = new Array(
+        "Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6",
+        "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"
+    );
+
+    for (var i = 0; i < months.length; i++) {
+        if (i % 3 === 0) {
+            var row = document.createElement("tr");
+            tableBody.appendChild(row);
+        }
+        // var monthCal = document.querySelector("tbody");
+        var cell = document.createElement("td");
+        var div = document.createElement("div");
+        cell.className = "month-item";
+        div.className = "month-content"
+        div.textContent = months[i];
+        cell.appendChild(div);
+        row.appendChild(cell);
+    }
+    
+    var monthYearElement = document.querySelector(".tab");
+    monthYearElement.innerHTML = "";
+    monthYearElement.appendChild(monthTable);
+}
+
+function showYearTable() {
+    var yearTable = document.createElement("div");
+    yearTable.className = "year-table";
+    yearTable.innerHTML = "<table><tbody></tbody></table>";
+    var tableBody = yearTable.querySelector("tbody");
+    var startYear = currentYear - 7; // lấy năm trước năm hiện tại
+    var endYear = currentYear + 7;
+    var yearCount = 0;
+    var row = document.createElement("tr");
+    tableBody.appendChild(row);
+
+    for (var i = startYear ; i <= endYear; i++) {
+        if (yearCount % 3 === 0 &&  yearCount !== 0) 
+        {
+            row = document.createElement("tr");
+            tableBody.appendChild(row);
+        }
+        var cell = document.createElement("td");
+        var div = document.createElement ("div");
+        cell.className = "year-item";
+        div.className = "year-content";
+        div.textContent = i;
+        cell.appendChild(div);
+        row.appendChild(cell);
+        yearCount++;
+    }
+
+    var monthYearElement = document.querySelector(".tab");
+    monthYearElement.innerHTML = "";
+    monthYearElement.appendChild(yearTable);
 }
 //lấy số ngày của tháng
 function getDaysInMonth() {
-  var lastdayofMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-  return lastdayofMonth;
+    var lastdayofMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    return lastdayofMonth;
 }
 //lấy ngày bắt đầu của tháng
 function getStartDayInMonth() {
-  var startday = new Date(currentYear, currentMonth, 1).getDay();
-  return startday;
+    var startday = new Date(currentYear, currentMonth, 1).getDay();
+    return startday;
 }
 
+
+var monthYearElement = document.getElementsByClassName("current-date");
+for (var i = 0; i < monthYearElement.length; i++) {
+  monthYearElement[i].textContent = new Date(currentYear, currentMonth).toLocaleString('default', { month: 'long', year: 'numeric' });
+}
+
+
 function activeCurrentDay(day) {
-  var day1 = new Date().toDateString();
-  var day2 = new Date(currentYear, currentMonth, day).toDateString();
-  return day1 == day2 ? "current-value" : "";
+    var day1 = new Date().toDateString();
+    var day2 = new Date(currentYear, currentMonth, day).toDateString();
+    return day1 == day2 ? "current-value" : "";
 }
 
 function renderDate() {
-  var dateCL = document.querySelector("tbody");
-  var daysInMonth = getDaysInMonth();
-  var startDay = getStartDayInMonth();
-var daysFirstDate = getFirstDayMonth();
-dateCL.innerHTML = '';
+    var dateCL = document.querySelector("tbody");
+    var daysInMonth = getDaysInMonth();
+    var startDay = getStartDayInMonth();
+    dateCL.innerHTML = '';
 
-for (var i = 0; i < startDay; i++) {
-  var tr = document.createElement("tr");
-  var td = document.createElement("td");
-  td.className = "prev-month";
-  if (i === 0 || i % 7 === 0)
-  {
-    tr.appendChild(td);
-    dateCL.appendChild(tr);
-  }
-  var div = document.createElement("div");
-  div.className = "day-content";
-  div.textContent = i + 1;
-  td.appendChild(div);
-  dateCL.appendChild(td);
-}
+    for (var i = 0; i < startDay; i++) {
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        td.className = "prev-month";
+        if (i === 0 || i % 7 === 0) {
+            tr.appendChild(td);
+            dateCL.appendChild(tr);
+        }
+        var div = document.createElement("div");
+        div.className = "day-content";
+        div.textContent = i + 1;
+        td.appendChild(div);
+        dateCL.appendChild(td);
+    }
 
-  for (var i = 0; i < daysInMonth; i++) {
-    var td = document.createElement("td");
-    var div = document.createElement("div");
-    div.className ="day-content";
-    div.textContent = i + 1;
-    td.appendChild(div);
-    dateCL.appendChild(td);
-    if (i === 0 || i % 7 === 0)
-    {
-      var tr = document.createElement("tr");
-      dateCL.appendChild(tr);
+    for (var i = 0; i < daysInMonth; i++) {
+        var td = document.createElement("td");
+        var div = document.createElement("div");
+        div.className = "day-content";
+        div.textContent = i + 1;
+        td.appendChild(div);
+        dateCL.appendChild(td);
+        if (i === 0 || i % 7 === 0) {
+            var tr = document.createElement("tr");
+            dateCL.appendChild(tr);
+        }
+        if (activeCurrentDay(i + 1)) {
+            td.className += ("current-value");
+        }
     }
-    if (activeCurrentDay(i + 1))
-    {
-      td.className += ("current-value");
-    }
-  }
+    
 }
 // var btnPrevMonth = document.getElementsByClassName("prev-month-left");
 // btnPrevMonth.addEventListener("click", function() {
@@ -270,4 +338,4 @@ for (var i = 0; i < startDay; i++) {
 //   displayInfo();
 // });
 
-// window.onload = displayInfo;
+window.onload = displayInfo;
