2270e801051cc5284627076b2c879419c5ce5f0b
HungNguyen12184 <135783013+HungNguyen12184@users.noreply.github.com>
2023-08-22 22:17:26 +0700

    22/08

--- serverLPR.js
+++ serverLPR.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs'); // file system
 const app = express();
 const path = require('path');
-const cors = require('cors');
+var cors = require('cors');
 const port = 3000;
 app.use(cors());
-const imageDir = '/Users/lieunguyen/Documents/image';
 
+const imageDir = path.join(__dirname, 'src', 'public');
 app.get('/api/image/:imageName', (req, res) => {
     const imageName = req.params.imageName;
     const imagePath = path.join(imageDir, imageName);
-
+    console.log(imagePath);
     fs.readFile(imagePath, (err, data) => {
         if (err) {
             res.status(404).json({ error: 'Image not found.' });
