d20b30abb0cce61aad1ef75761534d3a950da9bf
HungNguyen12184 <135783013+HungNguyen12184@users.noreply.github.com>
2023-09-05 22:06:24 +0700

    0509
    2

--- serverLPR.js
+++ serverLPR.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mysql = require('mysql2');
-
 app.use(cors());
 
 const connection = mysql.createConnection({
